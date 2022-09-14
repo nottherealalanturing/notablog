94615027fed937555ce692ad7c9199d7fb600118
Assad Isah <nottherealalanturing@gmail.com>
2022-09-14 19:21:17 +0100

    feat: add custom methods to models

---
+++ db/migrate/20220914155847_add_constraints_to_counters.rb
@@ -0,0 +1,12 @@
+class AddConstraintsToCounters < ActiveRecord::Migration[7.0]
+  def change
+        change_column_null :posts, :comments_counter, false
+        change_column_default :posts, :comments_counter, 0
+    
+        change_column_null :posts, :likes_counter, false
+        change_column_default :posts, :likes_counter, 0
+    
+        change_column_null :users, :posts_counter, false
+        change_column_default :users, :posts_counter, 0 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_13_093647) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_14_155847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_093647) do
     t.datetime "updated_at", null: false
     t.bigint "author_id"
     t.bigint "post_id"
-    t.index ["author_id"], name: "index_comments_on_author_id", unique: true
-    t.index ["post_id"], name: "index_comments_on_post_id", unique: true
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -29,26 +29,26 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_093647) do
     t.datetime "updated_at", null: false
     t.bigint "author_id"
     t.bigint "post_id"
-    t.index ["author_id"], name: "index_likes_on_author_id", unique: true
-    t.index ["post_id"], name: "index_likes_on_post_id", unique: true
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "comments_counter"
-    t.integer "likes_couter"
+    t.integer "comments_counter", default: 0, null: false
+    t.integer "likes_counter", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
-    t.index ["author_id"], name: "index_posts_on_author_id", unique: true
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
