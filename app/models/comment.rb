3f0376f66b83329ab34b41a531fbae6f07324840
Assad Isah <nottherealalanturing@gmail.com>
2022-09-14 19:21:54 +0100

    fix: resolve linter errors with rubocop

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
 
-    def update_comments_counter
-        post.update(comments_counter: post.comments.count)
-    end
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
