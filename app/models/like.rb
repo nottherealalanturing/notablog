03ffeb2e9d758e6131fd5f815d96dc9352b2e582
Assad Isah <nottherealalanturing@gmail.com>
2022-09-14 15:50:59 +0100

    fix: correct spelling for class name in association

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: "user", foreign_key: "author"
+    belongs_to :author, class_name: "User", foreign_key: "author"
     belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: "user", foreign_key: "author"
+    belongs_to :author, class_name: "User", foreign_key: "author"
     has_many :comments
     has_many :likes
 end
