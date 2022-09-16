6f2b23aad41c1282704db3355e67bd197b6b79e9
Assad Isah <nottherealalanturing@gmail.com>
2022-09-16 16:50:59 +0100

    fix: resolve linter errors with rubocop

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,40 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:user) {User.create(name: "Test User", bio: "Test Bio", photo: "Test Image Url")}
-  let(:blog) {Post.create(author: user, title: "New Post", text: "Lorem Ipsum")}
-  subject {described_class.new}
-  
-  describe "Validation" do
-    it "should not be valid without a user_id or text" do
+  let(:user) { User.create(name: 'Test User', bio: 'Test Bio', photo: 'Test Image Url') }
+  let(:blog) { Post.create(author: user, title: 'New Post', text: 'Lorem Ipsum') }
+  subject { described_class.new }
+
+  describe 'Validation' do
+    it 'should not be valid without a user_id or text' do
       subject.author = nil
       subject.post = blog
       subject.text = nil
       expect(subject).to_not be_valid
     end
 
-    it "should not be valid without a post_id" do
+    it 'should not be valid without a post_id' do
       subject.author = user
       subject.post = nil
-      subject.text = "Fake Comment"
+      subject.text = 'Fake Comment'
       expect(subject).to_not be_valid
     end
 
-    it "it should be valid" do
-      subject = described_class.create(author: user, post: blog, text: "Fake Comment")
+    it 'it should be valid' do
+      subject = described_class.create(author: user, post: blog, text: 'Fake Comment')
       expect(subject).to be_valid
     end
   end
 
-  describe "Add Comment to post" do  
-    it "should add a new comment to post" do 
-      subject = described_class.create(author: user, post: blog, text: "Fake Comment")
+  describe 'Add Comment to post' do
+    it 'should add a new comment to post' do
+      subject = described_class.create(author: user, post: blog, text: 'Fake Comment')
       expect(subject.post.comments_counter).to eq(1)
     end
   end
 
-  describe "Associations" do
-    it {should belong_to(:author).class_name(:User).with_foreign_key(:author_id)}
-    it {should belong_to(:post)}
+  describe 'Associations' do
+    it { should belong_to(:author).class_name(:User).with_foreign_key(:author_id) }
+    it { should belong_to(:post) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject { described_class.new(name: 'Test User', bio: 'Test Bio', photo: 'Test Image Url') }
 
-  subject {described_class.new(name: "Test User", bio: "Test Bio", photo: "Test Image Url")}
-  
-  describe "Validations" do
-    it "is valid with attributes" do
+  describe 'Validations' do
+    it 'is valid with attributes' do
       expect(subject).to be_valid
     end
 
-    it "is not valid without name" do
+    it 'is not valid without name' do
       subject.name = nil
       expect(subject).to_not be_valid
     end
 
-    it "is not valid if posts_counter is set to a none integer value" do
-      subject.posts_counter = "nil"
+    it 'is not valid if posts_counter is set to a none integer value' do
+      subject.posts_counter = 'nil'
       expect(subject).to_not be_valid
     end
   end
 
-  describe "Associations" do
-    it {should have_many(:comments).with_foreign_key(:author_id).dependent(:destroy)}
-    it {should have_many(:posts).with_foreign_key(:author_id).dependent(:destroy)}
-    it {should have_many(:likes).with_foreign_key(:author_id).dependent(:destroy)}
+  describe 'Associations' do
+    it { should have_many(:comments).with_foreign_key(:author_id).dependent(:destroy) }
+    it { should have_many(:posts).with_foreign_key(:author_id).dependent(:destroy) }
+    it { should have_many(:likes).with_foreign_key(:author_id).dependent(:destroy) }
   end
-  
 end
-
