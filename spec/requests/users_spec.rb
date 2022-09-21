1622d9ef306aaf3da0ac789ec8494014f3003d07
Assad Isah <nottherealalanturing@gmail.com>
2022-09-21 16:00:30 +0100

    test: add request test for users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_index_path
-      expect(response).to have_http_status(200)
+  describe "GET /index" do
+    before(:each) { get users_path}
+    
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders the index page" do
+      expect(response).to render_template(:index)
+    end
+
+    it "should include 'Here is a list of users'" do
+      expect(response.body).to include("Here is a list of users")
+    end
+  end
+
+  describe "GET /show" do
+    before(:each) { get user_path id: 1}
+
+    it "is a success" do 
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders the index page" do
+      expect(response).to render_template(:show)
+    end
+
+    it "should include 'Show single user information'" do
+      expect(response.body).to include "Show single user information"
     end
   end
 end
+
