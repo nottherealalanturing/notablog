566e84ac73d637411e2e9a3b02684f778c883cc2
Assad Isah <nottherealalanturing@gmail.com>
2022-09-21 16:01:21 +0100

    fix: resolve linter errors with rubocop

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,35 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    before(:each) { get user_posts_path user_id: 1}
-    
-    it "is a success" do
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    before(:each) { get user_posts_path user_id: 1 }
+
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders the index page" do
+    it 'renders the index page' do
       expect(response).to render_template(:index)
     end
 
     it "should include 'Here is a list of posts for a given user'" do
-      expect(response.body).to include("Here is a list of posts for a given user")
+      expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
 
-  describe "GET /show" do
-    before(:each) { get user_post_path user_id: 1, id: 1}
+  describe 'GET /show' do
+    before(:each) { get user_post_path user_id: 1, id: 1 }
 
-    it "is a success" do 
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders the index page" do
+    it 'renders the index page' do
       expect(response).to render_template(:show)
     end
 
     it "should include 'Show single post information'" do
-      expect(response.body).to include "Show single post information"
+      expect(response.body).to include 'Show single post information'
     end
   end
 end
