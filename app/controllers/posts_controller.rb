9edbd3109275a46a09321ac1b90ecc6d70a0db98
Assad Isah <nottherealalanturing@gmail.com>
2022-09-23 01:12:52 +0100

    feat: current logged in user can add a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,36 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = get_user_id params[:user_id]
     @posts = @user.posts.includes((:comments))
   end
 
   def show
-    @user = User.find(params[:user_id])
+    @user = get_user_id params[:user_id]
     @post = @user.posts.includes(comments: [:author]).find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    new_post = Post.new(author: current_user, text: params[:post][:text], title: params[:post][:title]);
+    puts new_post.text
+    puts new_post.title
+    puts new_post.author.name
+    puts new_post.valid?
+    if new_post.save
+      redirect_to post_path(new_post.id)
+    end
+  end
+
+  private
+    def get_user_id(user_id)
+      if user_id
+        User.find(params[:user_id])
+      else
+        current_user
+      end
+    end
+  
 end
