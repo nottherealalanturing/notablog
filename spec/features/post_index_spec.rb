91d5041cb826cb74c995022f90fa2d843b89b228
Assad Isah <nottherealalanturing@gmail.com>
2022-09-26 17:29:12 +0100

    fix: resovle linter errors with rubocop

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,34 +1,36 @@
 require 'rails_helper'
 
-RSpec.feature "Post Index Page", type: :feature do
+RSpec.feature 'Post Index Page', type: :feature do
   before :each do
-    @user = User.create(name: 'Lilly', photo: "https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", bio: 'Teacher from Poland.')
-    @user2 = User.create(name: 'Tom', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', bio: 'Teacher from Poland.')
+    @user = User.create(name: 'Lilly',
+                        photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', bio: 'Teacher from Poland.')
+    @user2 = User.create(name: 'Tom',
+                         photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', bio: 'Teacher from Poland.')
     @first_post = Post.create(author: @user, title: 'Hello', text: 'This is my first post')
     5.times do |_i|
       Comment.create(post: @first_post, author: @user2, text: 'Hi Lilly!')
     end
-  
+
     visit user_posts_path(@user)
   end
-  
-  it "displays user profile picture" do
-    expect(page.find(:css, ".post_page_image")[:src]).to have_content(@user.photo)
+
+  it 'displays user profile picture' do
+    expect(page.find(:css, '.post_page_image')[:src]).to have_content(@user.photo)
   end
 
-  it "displays number of posts" do
+  it 'displays number of posts' do
     expect(page).to have_content('Number of posts: 1')
   end
 
-  it "displays the user's username " do 
+  it "displays the user's username " do
     expect(page).to have_content(@user.name)
   end
 
-  it "displays the post title" do 
+  it 'displays the post title' do
     expect(page).to have_content(@first_post.title)
   end
 
-  it "displays the post body" do 
+  it 'displays the post body' do
     expect(page).to have_content(@first_post.text)
   end
 
@@ -48,5 +50,4 @@ RSpec.feature "Post Index Page", type: :feature do
     click_link(@first_post.title)
     expect(page).to have_content("#{@first_post.comments_counter} comment")
   end
-
 end
