01135389b1818deb55608bec4e10cf9ab9a19483
charlotte divine <divinemaina@gmail.com>
2022-09-26 20:56:53 -0700

    add the  test  show user's profile picture

---
+++ spec/features/user_show_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+RSpec.feature 'User Index Page', type: :feature do
+    before :each do
+        @user = User.create(name: 'Lilly',
+                            photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', bio: 'Teacher from Poland.')
+        @user2 = User.create(name: 'Tom',
+                             photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', bio: 'Teacher from Poland.')
+                             
+
+         visit user_path(@user.id)
+    
+    end
+    it "should  show user's profile picture" do
+        expect(page.find(:css, '.user_page_image')[:src]).to have_content(@user.photo)
+    end
+end
