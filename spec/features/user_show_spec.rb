504c6c67da4f031414fddc29575bb45f7a49ba59
charlotte divine <divinemaina@gmail.com>
2022-09-26 21:05:49 -0700

    add the  test to show the number of posts the user has written

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -5,7 +5,10 @@ RSpec.feature 'User Index Page', type: :feature do
                             photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', bio: 'Teacher from Poland.')
         @user2 = User.create(name: 'Tom',
                              photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', bio: 'Teacher from Poland.')
-                             
+        @first_post = Post.create(author: @user, title: 'Hello', text: 'This is my first post')
+    5.times do |_i|
+      Comment.create(post: @first_post, author: @user2, text: 'Hi Lilly!')
+    end                     
 
          visit user_path(@user.id)
     end
@@ -16,5 +19,9 @@ RSpec.feature 'User Index Page', type: :feature do
 
     it "should show the user's username." do
         expect(page).to have_content('Lilly')
+    end
+
+    it "should show the number of posts the user has written." do
+        expect(page).to have_content('Number of posts: 1')
     end
 end
