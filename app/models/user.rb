7e95f2f25f24746956edd8eb5a4e630d95a9ea6d
Assad Isah <nottherealalanturing@gmail.com>
2022-09-28 17:38:05 +0100

    feat: add devise recomfirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Include default devise modules. Others available are: , :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :likes, foreign_key: :author_id, dependent: :destroy
